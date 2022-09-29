6db97ffd32765486dd16bd6ce42423303804231d
Kolja Krumbacher <kolja.krumbacher@lsdom.brechtschulen.de>
2022-09-29 11:02:27 +0200

    zweites Update

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+bool isRunning = false;
+char answer[4];
+
+class Player {
+
+    int x = 0;
+    int y = 0;
+
+    void moveX() {
+        x += 1;
+    }
+
+    void moveY() {
+        y += 1;
+    }
+
+    void moveX_() {
+        x += -1;
+    }
+
+    void moveY_() {
+        y += -1;
+    }
+
+};
+
+
+
+int main() {
+    
+
+
+
+
+}
